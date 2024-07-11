27eb6994280ba51bd8565c22b1144d61159bbcb9
Kashish-09 <shahkashish953@gmail.com>
2024-07-12 02:08:18 +0530

    reactplayer added

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,69 +1,73 @@
-import {useState} from 'react'
-import {VideoPlayer} from './VideoPlayer.jsx'
+import { useState } from 'react';
+import ReactPlayer from 'react-player';
+
 export default function ExerciseCard(props) {
-    const{exercise, i} = props
-    const [setsCompleted, setSetsCompleted] = useState(0)
-    function handleSetIncrement(){
-        setSetsCompleted((setsCompleted + 1) % 6)
-    }
-    let j=2500
+  const { exercise, i } = props;
+  const [setsCompleted, setSetsCompleted] = useState(0);
+
+  function handleSetIncrement() {
+    setSetsCompleted((setsCompleted + 1) % 6);
+  }
+
+  let j = 2500;
+
   return (
     <div className='p-4 rounded-md flex flex-col gap-4 bg-slate-950 sm:flex-wrap'>
-        <div className='flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4'>
-            <h4 className='text-3xl hidden sm:inline sm:text-4xl md:text-5xl font-semibold text-slate-400'>
-                0{i + 1}
-            </h4>
-            <h2 className='capitalize whitespace-nowrap truncate max-w-full text-lg sm:text-xl md:text-2xl flex-1 sm:text-center'>
-                {exercise.name.replaceAll("_", " ")}
-            </h2>
-            <p className='text-sm text-slate-400 capitalize'>
-                {exercise.type}
-            </p>
-        </div>
-        <div className='flex flex-col'>
-            <h3 className='text-slate-400 text-sm'>
-                Muscle Groups
-            </h3>
-            <p className='capitalize'>
-                {exercise.muscles.join(' & ')}
-            </p>
-        </div>
-        <div className='flex flex-col bg-slate-950 rounded gap-2'>
-            {exercise.description.split('___').map((val) => {
-                return(
-                    <div key={j++} className='text-sm'>
-                        {val}
-                    </div>
-                )
-            })}
-        </div>
-        <div className='grid-cols-2 sm:grid-cols-4 sm:palce-items-center gap-2'>
-            {['reps','rest','tempo','link'].map(info =>{
-                return(
-                <div key={info} className='flex flex-col p-2 rounded border-[1.5x] border-solid border-slate-900 w-full'>
-                    <div>
-                        <h3 className='capitalize text-slate-400 text-sm'>
-                            {info === 'reps' ? `${exercise.unit}` : info}
-                        </h3>
-                        <p className='font-md'>
-                            {exercise[info]}
-                        </p>
-                    </div>
-                    <div>
-                    <VideoPlayer videoId={'rT7DgCr-3pg'} />
-                    </div>
-                </div>
-                )
-            })}
-            <button onClick={handleSetIncrement} className='flex flex-col p-2 rounded border-[1.5px] duration-200 border-solid border-blue-900 hover:border-blue-600 w-full'>
-                <h3 className='text-slate-400 text-sm capitalize'>
-                    Sets Completed
+      <div className='flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4'>
+        <h4 className='text-3xl hidden sm:inline sm:text-4xl md:text-5xl font-semibold text-slate-400'>
+          0{i + 1}
+        </h4>
+        <h2 className='capitalize whitespace-nowrap truncate max-w-full text-lg sm:text-xl md:text-2xl flex-1 sm:text-center'>
+          {exercise.name.replaceAll('_', ' ')}
+        </h2>
+        <p className='text-sm text-slate-400 capitalize'>
+          {exercise.type}
+        </p>
+      </div>
+      <div className='flex flex-col'>
+        <h3 className='text-slate-400 text-sm'>Muscle Groups</h3>
+        <p className='capitalize'>{exercise.muscles.join(' & ')}</p>
+      </div>
+      <div className='flex flex-col bg-slate-950 rounded gap-2'>
+        {exercise.description.split('___').map((val) => (
+          <div key={j++} className='text-sm'>
+            {val}
+          </div>
+        ))}
+      </div>
+      <div className='flex w-full'>
+        <div className='w-1/2 p-4'>
+          {['reps', 'rest', 'tempo'].map((info) => (
+            <div key={info} className='flex flex-col p-2 rounded border-[1.5px] border-solid border-slate-900 w-full mb-2'>
+              <div>
+                <h3 className='capitalize text-slate-400 text-sm'>
+                  {info === 'reps' ? `${exercise.unit}` : info}
                 </h3>
-                <p className='font-medium'>
-                    {setsCompleted}/5
-                </p>
-            </button>
+                <p className='font-md'>{exercise[info]}</p>
+              </div>
+            </div>
+          ))}
         </div>
+        <div className='w-1/2 p-4 flex items-center justify-center'>
+          <div className='w-full h-full p-2 rounded border-[1.5px] border-solid border-slate-200'>
+            <ReactPlayer
+              className='react-player'
+              url={`https://www.youtube.com/watch?v=${exercise.link}`}
+              width='100%'
+              height='100%'
+            />
+          </div>
+        </div>
+      </div>
+      <div className=''>
+        <button
+          onClick={handleSetIncrement}
+          className='flex flex-col p-2 rounded border-[1.5px] duration-200 border-solid border-blue-900 hover:border-blue-600 w-full'
+        >
+          <h3 className='text-slate-400 text-sm capitalize'>Sets Completed</h3>
+          <p className='font-medium'>{setsCompleted}/5</p>
+        </button>
+      </div>
     </div>
-  )
+  );
 }
